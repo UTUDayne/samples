16c1dfedcce8f0c37ee20a52e01bdf706618492e
Dayne Wyler <dayne.wyler@utahtech.edu>
2025-09-18 10:15:26 -0600

    Small changes

--- trails/client/app.js
+++ trails/client/app.js
@@ -1,20 +1,24 @@
 console.log("connected")
 
 let trail_button = querySelector("#trail_button")
+const trail_div = querySelector("#trail_div")
+load()
 
-fetch("http://localhost:5500/trails")
-.then(function(response){
-	response.json()
-	.then(function(data) {
-		console.log(data)
-		add_div_with_trail(data)
-		data.forEach(trail => add_div_with_trail(trail))
+function load(){
+	trail_div.innerHTML = ""
+	fetch("http://localhost:5500/trails")
+	.then(function(response){
+		response.json()
+		.then(function(data) {
+			console.log(data)
+			add_div_with_trail(data)
+			data.forEach(trail => add_div_with_trail(trail))
+		})
+
 	})
-
-})
+}
 
 function add_div_with_trail(trail){
-	let trail_div = document.querySelector("#trail_reviews")
 	let div = document.createElement("div")
 	let h3 = document.createElement("h3")
 	let p = document.createElement("p")
@@ -33,7 +37,18 @@ trail_button.onclick = function(){
 	console.log(name)
 
 	let data = "name="+encodeURIComponent(name)
-	//data += "description"+encodeURIComponent(description)
+	//data += "&description"+encodeURIComponent(description)
+
+	fetch("http://localhost:5500/trails", {method:"POST", body:data, headers:{"Content-Type":"application/x-www-form-urlencoded"}}).then(function(response){
+		response.json().then(function(data) {
+			console.log(data)
+			add_div_with_trail(data)
+			data.forEach(trail => add_div_with_trail(trail))
+
+			load()
+		})
+	})
+
 
 	console.log(data)
 
