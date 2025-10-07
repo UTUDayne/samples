8a9bb8461fb3ac80d0d28c06dec690f159242a72
Dayne Wyler <dayne.wyler@utahtech.edu>
2025-10-07 10:06:40 -0600

    IN-school db work

--- trails/server/app.py
+++ trails/server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from db import DB
 
 app = Flask(__name__)
 
@@ -7,15 +8,32 @@ trails = [
     {"name": "Dead horse point", "length": "80"},
     {"name": "delicate arch", "length": "805"}
 ]
+def dict_factory(cursor, row):
+    fields = []
+    # Extract column names from cursor description
+    for column in cursor.description:
+        fields.append(column[0])
+
+    # Create a dictionary where keys are column names and values are row values
+    result_dict = {}
+    for i in range(len(fields)):
+        result_dict[fields[i]] = row[i]
+
+    return result_dict
 
 @app.route("/trails", methods=["GET"])
 def hello_world():
+    db = DB("db.db")
+    trails = db.readall()
     return trails, {"Access-Control-Allow-Origin":"*"}
 
 @app.route("/trails", methods=["POST"])
 def home():
+    db = DB("db.db")
     print(request.form)
-    trails.append({"name":request.form['name']})
+    #trails.append({"name":request.form['name']})
+    record = {"name" : request.form["name"], "description" : request.form['description'], "length" : request.form['length'], "rating" : request.form["rating"]}
+    db.insert(request.form)
     return "Created", 201, {"Access-Control-Allow-Origin":"*"}
 
 def main():
---
+++ trails/server/db.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+connection = sqlite3.connect("db.db")
+cursor = connection.cursor()
+
+
+
+fields = ("'name', 'description', 'length', 'rating'")
+test_values = ("'blurt', 'blue', 4, 4")
+
+cursor.execute("INSERT INTO trails " + fields + " " + test_values)
+connection.commit()
+
+
+
+class DB:
+    def __init__(self, filename):
+        self.filename = filename
+        self.connection = sqlite3.connection(self.filename)
+        self.cursor = self.connection.cursor
+    
+    def readall(self):
+        self.cursor.execute("SELECT * FROM trails")
+        records = self.cursor.fetchall()
+        all = {}
+        for row in rows:
+            d = dict_factory(self.cursor, row)
+            all.append(d)
+        print(records)
+        return all
+    
+    def insert(self, data):
+        self.cursor.execute("INSERT INTO " + )
+        self.connection.commit
+
+    def closeDB(self):
+        self.connection.close()
+
+if __name__ == "__main__":
+    db = DB(self, "db.db")
+    db.readall()
+    db.insert(record)
+    db.close()
